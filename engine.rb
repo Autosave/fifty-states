3dd15684f6ac0fbc900bbcb0ef5187ec2bc6cd17
Justin West <justinraywest@mail.com>
2015-05-01 09:33:09 -0500

    initial commit

---
+++ engine.rb
@@ -0,0 +1,140 @@
+$state_data = {
+                        "Alabama" => ["AL", "12/14/1819",  52423, "Montgomery", "Yellowhammer State",  4833722,  "Yellowhammer",  "Carmellia"],
+                        "Alaska" => ["AK", "01/03/1959", 656425, "Juneau", "The Last Frontier", 735132, "Willow Ptarmigan", "Forget Me Not"],
+                        "Arizona" => ["AZ", "02/14/1912", 114006, "Phoenix", "The Grand Canyon State", 6626624, "Cactus Wren", "Saguaro Cactus Blossom"],
+                        "Arkansas" => ["AR", "06/15/1836", 53182, "Little Rock", "The Natural State",  2959373, "Mockingbird", "Apple Blossom"],
+                        "California" => ["CA", "09/09/1850", 163707, "Sacramento", "Golden State", 38332521, "California Valley Quail", "California Poppy"],
+                        "Colorado" => ["CO", "08/01/1876", 104100, "Denver", "Centennial State", 5268367, "Lark Bunting", "Rocky Mountain Columbine"],
+                        "Connecticut" => ["CT", "01/09/1788", 5544, "Hartford", "Constitution State", 3596080, "Robin", "Mountain Laurel"],
+                        "Delaware" => ["DE", "12/07/1787", 2489, "Dover", "The First State", 925749, "Blue Hen Chicken", "Peach Blossom"],
+                        "Florida" => ["FL", "03/03/1845", 65758, "Tallahassee", "Sunshine State", 19552860, "Mockingbird", "Orange Blossom"],
+                        "Georgia" => ["GA", "01/02/1788", 59441, "Atlanta", "Peach State", 9992167, "Brown Thrasher", "Cherokee Rose"],
+                        "Hawaii" => ["HI", "08/21/1959", 10932, "Honolulu", "Aloha State", 1404054, "Nene", "Hibiscus"],
+                        "Idaho" => ["ID", "07/03/1890", 83574, "Boise", "Gem State", 1612136, "Mountain Bluebird", "Mock Orange"],
+                        "Illinois" => ["IL", "12/03/1818", 57918, "Springfield", "Prairie State", 12882135, "Cardinal", "Purple Violet"],
+                        "Indiana" => ["IN", "12/11/1816", 36420, "Indianapolis", "Hoosier State", 6570902, "Cardinal", "Peony"],
+                        "Iowa" => ["IA", "12/28/1846", 56276, "Des Moines", "Hawkeye State", 3090416, "Eastern Goldfinch", "Wild Prairie Rose"],
+                        "Kansas" => ["KS", "01/29/1861", 82282, "Topeka", "Sunflower State", 2893957, "Western Meadowlark", "Sunflower"],
+                        "Kentucky" => ["KY", "06/01/1792", 40411, "Frankfort", "Bluegrass State", 4395295, "Cardinal", "Goldenrod"],
+                        "Louisiana" => ["LA", "04/30/1812", 51843, "Baton Rouge", "Pelican State", 4625470, "Eastern Brown Pelican", "Magnolia"],
+                        "Maine" => ["ME", "03/15/1820", 35387, "Augusta", "Pine Tree State", 1328302, "Chickadee", "White Pine Cone And Tassel"],
+                        "Maryland" => ["MD", "04/28/1788", 12407, "Annapolis", "Old Line State", 5928814, "Baltimore Oriole", "Black Eyed Susan"],
+                        "Massachusetts" => ["MA", "02/06/1788", 10555, "Boston", "Bay State", 6692824, "Chickadee", "Mayflower"],
+                        "Michigan" => ["MI", "01/26/1837", 96810, "Lansing", "The Great Lakes State", 9895622, "Robin", "Apple Blossom"],
+                        "Minnesota" => ["MN", "05/11/1858", 86943, "Saint Paul", "North Star State", 5420380, "Common Loon", "Pink And White Lady's Slipper"],
+                        "Mississippi" => ["MS", "12/10/1817", 48434, "Jackson", "Magnolia State", 2991207, "Mockingbird", "Magnolia"],
+                        "Missouri" => ["MO", "08/10/1821", 69709, "Jefferson City", "Show Me State", 6044171, "Bluebird", "Hawthorn"],
+                        "Montana" => ["MT", "11/08/1889", 147046, "Helena", "Treasure State", 1015165, "Western Meadowlark", "Bitterroot"],
+                        "Nebraska" => ["NE", "03/01/1867", 77358, "Lincoln", "Cornhusker State", 1868516, "Western Meadowlark", "Goldenrod"],
+                        "Nevada" => ["NV", "10/31/1864", 110567, "Carson City", "The Silver State", 2790136, "Mountain Bluebird", "Sagebrush"],
+                        "New Hampshire" => ["NH", "06/21/1788", 9351, "Concord", "Granite State", 1323459, "Purple Finch", "Purple Lilac"],
+                        "New Jersey" => ["NJ", "12/18/1787", 8722, "Trenton", "Garden State",  8899339, "Eastern Goldfinch", "Violet"],
+                        "New Mexico" => ["NM", "01/06/1912", 121593, "Santa Fe", "Land Of Enchantment", 2085287, "Roadrunner", "Yucca Flower"],
+                        "New York" => ["NY", "07/26/1788", 54475, "Albany", "Empire State", 19651127, "Eastern Bluebird", "Rose"],
+                        "North Carolina" => ["NC", "11/21/1789", 53821, "Raleigh", "Old North State", 9848060, "Cardinal", "Dogwood"],
+                        "North Dakota" => ["ND", "11/02/1889", 70704, "Bismarck", "Peace Garden State", 723393, "Western Meadowlark", "Wild Prairie Rose"],
+                        "Ohio" => ["OH", "03/01/1803", 44828, "Columbus", "Buckeye State", 11570808, "Cardinal", "Scarlet Carnation"],
+                        "Oklahoma" => ["OK", "11/16/1907", 69903, "Oklahoma City", "Sooner State", 3850568, "Scissor Tailed Flycatcher", "Mistletoe"],
+                        "Oregon" => ["OR", "02/14/1859", 98386, "Salem", "Beaver State", 3930065, "Western Meadowlark", "Oregon Grape"],
+                        "Pennsylvania" => ["PA", "12/12/1787", 46058, "Harrisburg", "Keystone State", 12773801, "Ruffled Grouse", "Mountain Laurel"],
+                        "Rhode Island" => ["RI", "05/29/1790", 1545, "Providence", "The Ocean State", 1051511, "Rhode Island Red", "Violet"],
+                        "South Carolia" => ["SC", "05/23/1788", 32007, "Columbia", "Palmetto State", 4774839, "Carolina Wren", "Yellow Jessamine"],
+                        "South Dakota" => ["SD", "11/02/1889", 77121, "Pierre", "Mount Rushmore State", 844877, "Ring Necked Pheasant", "Pasque Flower"],
+                        "Tennessee" => ["TN", "06/01/1796", 42146, "Nashville", "Volunteer State", 6495978, "Mockingbird", "Iris"],
+                        "Texas" => ["TX", "12/29/1845", 268601, "Austin", "Lone Star State", 26448193, "Mockingbird", "Bluebonnet"],
+                        "Utah" => ["UT", "01/04/1896", 84904, "Salt Lake City", "The Beehive State", 2900872, "Ring Billed Gull", "Sego Lily"],
+                        "Vermont" => ["VT", "03/04/1791", 9615, "Montpelier", "Green Mountain State", 626630, "Hemit Thrush", "Red Clover"],
+                        "Virginia" => ["VA", "06/25/1788", 42769, "Richmond", "Old Dominion State", 8260405, "Cardinal", "Dogwood"],
+                        "Washington" => ["WA", "11/11/1889", 71303, "Olympia", "The Evergreen State", 6971406, "Eastern Goldfinch", "Pink Rhododendron"],
+                        "West Virginia" => ["WV", "06/20/1863", 24231, "Charleston", "Mountain State", 1854304, "Cardinal", "Rhododendron"],
+                        "Wisconsin" => ["WI", "05/29/1848", 65503, "Madison", "Badger State", 5742713, "Robin", "Wood Violet"],
+                        "Wyoming" => ["WY", "07/10/1890", 97818, "Cheyenne", "Equality State", 582658, "Western Meadowlark", "Indian Paintbrush"]
+}
+
+class State
+    def initialize(name)
+      @name = name
+      @abbreviation = $state_data[@name][0]
+      @statehood = $state_data[@name][1]
+      @area = $state_data[@name][2]
+      @capital = $state_data[@name][3]
+      @nickname = $state_data[@name][4]
+      @population = $state_data[@name][5]
+      @bird = $state_data[@name][6]
+      @flower = $state_data[@name][7]
+    end
+end
+
+finished = false
+while  not finished do
+  puts "What state do you want to enter information about?"
+  state_name = gets.chomp
+  if $state_data.key?(state_name)
+    current_state = State.new("#{state_name}")
+    puts "What is the abbreviation for #{state_name}?"
+    state_abbreviation = gets.chomp
+    if current_state.instance_variable_get(:@abbreviation) === state_abbreviation
+      puts "What was the date of statehood for #{state_name}? (00/00/0000)" 
+      state_statehood = gets.chomp 
+      if current_state.instance_variable_get(:@statehood) === state_statehood
+        puts "What is the approximate total area of #{state_name} in square miles?"
+        state_area = gets.chomp
+        if current_state.instance_variable_get(:@area) === state_area.to_i
+          puts "What is the capital of #{state_name}?"
+          state_capital = gets.chomp
+          if current_state.instance_variable_get(:@capital) === state_capital
+            puts "What is currently the most common nickname for #{state_name}?"
+            state_nickname = gets.chomp
+            if current_state.instance_variable_get(:@nickname) === state_nickname
+              puts "What is the approximate population of #{state_name}?"
+              state_population = gets.chomp
+              if current_state.instance_variable_get(:@population) === state_population.to_i
+                puts "What is the state bird of #{state_name}?"
+                state_bird = gets.chomp
+                if current_state.instance_variable_get(:@bird) === state_bird
+                  puts "What is the state flower of #{state_name}?"
+                  state_flower = gets.chomp
+                  if current_state.instance_variable_get(:@flower) === state_flower
+                    puts "You have completed the first portion of research on #{state_name}."
+                    file_name = "#{state_name}.txt"
+                    state_file = File.open(file_name, "w")
+                    state_file.puts "The abbreviation for #{state_name} is #{state_abbreviation}."
+                    state_file.puts "The date #{state_name} became a state was #{state_statehood}."
+                    state_file.puts "The area of #{state_name} is approximately #{state_area} square miles."
+                    state_file.puts "The capital of #{state_name} is #{state_capital}."
+                    state_file.puts "The most commonly used current nickname for #{state_name} is #{state_nickname}."
+                    state_file.puts "The approximate population for #{state_name} is #{state_population}."
+                    state_file.puts "The state bird of #{state_name} is #{state_bird}."
+                    state_file.puts "The state flower of #{state_name} is #{state_flower}."
+                    state_file.close
+                    finished = true
+                  else
+                    puts "Double check you didn't include any hyphens."
+                  end
+                else
+                  puts "Double check you didn't include any hyphens."
+                end
+              else
+                puts "Double check your number."
+              end
+            else
+              puts "Double check if it's currently used and the most common."
+            end
+          else
+            puts "Double check your spelling."
+          end
+        else
+          puts "Double check your number corresponds to square miles."
+        end
+      else 
+        puts "Double check your date or your formatting."
+      end
+    else
+      puts "Double check your research."
+    end
+  else
+    puts "Double check your spelling and try again."
+  end
+end
+
+
